dbe847b247b36fa1e0778836683e697e129ac511
kaiserpogi <baritaevron@gmail.com>
2025-10-28 08:35:16 +0800

    Add project files.

---
+++ BaritFTEProblem1/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtTitle = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.numPrice = New System.Windows.Forms.NumericUpDown()
+        Me.cmbCategory = New System.Windows.Forms.ComboBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblPrice = New System.Windows.Forms.Label()
+        Me.lblCategory = New System.Windows.Forms.Label()
+        CType(Me.numPrice, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtTitle
+        '
+        Me.txtTitle.Location = New System.Drawing.Point(190, 63)
+        Me.txtTitle.Name = "txtTitle"
+        Me.txtTitle.Size = New System.Drawing.Size(100, 20)
+        Me.txtTitle.TabIndex = 0
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(190, 107)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 20)
+        Me.txtName.TabIndex = 1
+        '
+        'numPrice
+        '
+        Me.numPrice.Location = New System.Drawing.Point(343, 83)
+        Me.numPrice.Name = "numPrice"
+        Me.numPrice.Size = New System.Drawing.Size(120, 20)
+        Me.numPrice.TabIndex = 2
+        '
+        'cmbCategory
+        '
+        Me.cmbCategory.FormattingEnabled = True
+        Me.cmbCategory.Items.AddRange(New Object() {"Painting", "Sculpture", "Digital Art", "Photography", "Crafts"})
+        Me.cmbCategory.Location = New System.Drawing.Point(190, 161)
+        Me.cmbCategory.Name = "cmbCategory"
+        Me.cmbCategory.Size = New System.Drawing.Size(121, 21)
+        Me.cmbCategory.TabIndex = 3
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(190, 189)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 4
+        Me.btnAdd.Text = "Add Artwork"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Location = New System.Drawing.Point(190, 44)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(69, 13)
+        Me.lblTitle.TabIndex = 5
+        Me.lblTitle.Text = "Artwork Title:"
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(193, 90)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(64, 13)
+        Me.lblName.TabIndex = 6
+        Me.lblName.Text = "Artist Name:"
+        '
+        'lblPrice
+        '
+        Me.lblPrice.AutoSize = True
+        Me.lblPrice.Location = New System.Drawing.Point(340, 63)
+        Me.lblPrice.Name = "lblPrice"
+        Me.lblPrice.Size = New System.Drawing.Size(73, 13)
+        Me.lblPrice.TabIndex = 7
+        Me.lblPrice.Text = "Artwork Price:"
+        '
+        'lblCategory
+        '
+        Me.lblCategory.AutoSize = True
+        Me.lblCategory.Location = New System.Drawing.Point(190, 142)
+        Me.lblCategory.Name = "lblCategory"
+        Me.lblCategory.Size = New System.Drawing.Size(91, 13)
+        Me.lblCategory.TabIndex = 8
+        Me.lblCategory.Text = "Artwork Category:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblCategory)
+        Me.Controls.Add(Me.lblPrice)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.cmbCategory)
+        Me.Controls.Add(Me.numPrice)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.txtTitle)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.numPrice, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtTitle As TextBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents numPrice As NumericUpDown
+    Friend WithEvents cmbCategory As ComboBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblPrice As Label
+    Friend WithEvents lblCategory As Label
+End Class
